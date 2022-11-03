9b486be73953e59ab9a2212363c9539e0372219e
Leo Rautio <leoaleksisrautio@gmail.com>
2022-11-03 09:54:42 +0200

    first commit

---
+++ src/TodoList.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
+import TodoTable from "./TodoTable";
+
+
+
+
+function TodoList() {
+    //  
+
+    const [todo, setTodo] = useState({description: "", date: ""});
+    const [todos, setTodos] = useState([]);
+    const columns = [{field: "description"},{field: "date"}]
+    
+    const inputChanged = (event) => {
+        setTodo({...todo, [event.target.name] : event.target.value});
+    }
+
+    const addTodo = (event) => {
+        event.preventDefault();
+        setTodos([...todos, todo]);
+        setTodo({ description: "", date: ""});
+    }
+
+    return (
+        <div>
+            <h1>Todolist</h1>
+            <input type="text" onChange={inputChanged} placeholder="Description" name="description" value={todo.description} />            
+            <input type="text" onChange={inputChanged} placeholder="Date" name="date" value={todo.date} />
+            <button onClick={addTodo}>Add</button>
+
+            <table>
+                <tr><td>Description</td><td>Date</td></tr>
+                <tbody>
+                {
+                todos.map((todo, index) => <tr key={index}><td>{todo.description}</td><td>{todo.date}</td><td>{todo.priority}</td></tr>)
+                }
+                </tbody>
+            </table> 
+        </div>
+    )
+
+}
+
+export default TodoList;
---
+++ src/TodoTable.js
@@ -0,0 +1,23 @@
+import { AgMenuItemComponent } from 'ag-grid-community';
+import React from 'react';
+
+function TodoTable(props) {
+    return (
+        <div>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Date</th>
+                        <th>Description</th>
+                    </tr>
+                    { props.todos.map((item, index) => 
+                    <tr key={index}>
+                        <td>{item.date}</td><td>{item.description}</td>
+                    </tr>)}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default TodoTable;
